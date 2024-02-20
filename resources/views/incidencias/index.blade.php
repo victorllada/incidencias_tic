6966593798ca53424f3bcc3d5591528f4348f70e
cesartg11 <cesar.torregonzalez@iesmiguelherrero.com>
2024-02-20 23:22:33 +0100

    Creacion InicioController, su respectiva ruta y crear vista incidencias.index Fixes #15

---
+++ resources/views/incidencias/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    Esto es ncidencias index
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IncidenciaController;
+use App\Http\Controllers\InicioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Inicio
+Route::get('/', InicioController::class)->name('inicio');
 
 //Incidencias
 Route::resource('incidencias', IncidenciaController::class)->parameters([
