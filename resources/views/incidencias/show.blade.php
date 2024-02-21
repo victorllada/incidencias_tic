82b0b2e9603104bed515f058317897134e810788
cesartg11 <cesar.torregonzalez@iesmiguelherrero.com>
2024-02-21 17:15:25 +0100

    Modificación de incindencias.index (vista) y creación de incidencias.show Fixes #21

---
+++ resources/views/incidencias/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.plantilla')
+@section('titulo', 'Incidencias - show')
+@section('contenido')
+
+    <h1>Incidencias show</h1>
+    <div>
+        <ul>
+            <li>Id:{{ $incidencia->id }}</li>
+            <li>Tipo:{{ $incidencia->tipo }}</li>
+            <li>Subtipo:{{ $incidencia->subtipo_id }}</li>
+            <li>Descripción:{{ $incidencia->descripcion }}</li>
+            <li>Estado:{{ $incidencia->estado }}</li>
+        </ul>
+    </div>
+@endsection
