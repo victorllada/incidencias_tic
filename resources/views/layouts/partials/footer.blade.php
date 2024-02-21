016603845a1ab3e1010bee348509d97d7c1f7d4d
Luis Concha Cabrales <lconchac01@educantabria.es>
2024-02-21 19:20:40 +0100

    creacion del footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,12 @@
-<footer>
-    ©Equipo 1
+<footer class="aquamarine-950 d-flex flex-column justify-content-center align-items-center">
+    <div class="d-flex justify-content-center flex-nowrap">
+        <div><a href="">Inicio</a></div>
+        <div><a href="">Crear incidencia</a></div>
+        <div><a href="">Administrar usuarios</a></div>
+        <div><a href="">Informes</a></div>
+    </div>
+
+    <div class="d-flex justify-content-center flex-nowrap">
+        <div class="text-white-50">©Equipo 1</div>
+    </div>
 </footer>
