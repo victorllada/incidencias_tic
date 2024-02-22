b59d14dbd7d6966eef4d5fba33a478e480bf629a
Hugocl01 <hugocayon@gmail.com>
2024-02-22 01:40:53 +0100

    Vista del index estilo Fixes #40

--- resources/views/incidencias/index.blade.php
+++ resources/views/incidencias/index.blade.php
@@ -4,15 +4,15 @@
 
     <h1>Incidencias index</h1>
     @forelse ($incidencias as $incidencia)
-        <div>
+        <div class="mb-4 lista-incidencias">
             <a href="{{ route('incidencias.show', $incidencia) }}">
-                <ul>
-                    <li>Id:{{ $incidencia->id }}</li>
-                    <li>Tipo:{{ $incidencia->tipo }}</li>
-                    <li>Subtipo:{{ $incidencia->subtipo_id }}</li>
-                    <li>Descripción:{{ $incidencia->descripcion }}</li>
-                    <li>Estado:{{ $incidencia->estado }}</li>
-                </ul>
+                <div class="row flex-nowrap">
+                    <div class="col border rounded-start-2 p-3">Id:{{ $incidencia->id }}</div>
+                    <div class="col border border-start-0 p-3">Tipo:{{ $incidencia->tipo }}</div>
+                    <div class="col border border-start-0 p-3">Subtipo:{{ $incidencia->subtipo_id }}</div>
+                    <div class="col col-6 border border-start-0 p-3">Descripción:{{ $incidencia->descripcion }}</div>
+                    <div class="col border border-start-0 rounded-end-2 p-3">Estado:{{ $incidencia->estado }}</div>
+                </div>
             </a>
         </div>
     @empty
