89b461c00453c142135461837ece7d4994c1e038
cesartg11 <cesar.torregonzalez@iesmiguelherrero.com>
2024-02-22 18:02:10 +0100

    Modificar metodo index en IncidenciaController para usar páginación Fixes #43

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('titulo')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
