30c0d11cbd41efafa6e82002570fb36510e53944
Hugocl01 <hugocayon@gmail.com>
2024-02-22 13:59:37 +0100

    Vista detallada de la incidencia

--- resources/views/incidencias/show.blade.php
+++ resources/views/incidencias/show.blade.php
@@ -6,23 +6,39 @@
     <div class="fs-3 mb-4">
         <a href="{{ route('incidencias.index', $incidencia) }}">Inicio</a> -> Incidencia {{ $incidencia->id }}
     </div>
+    <div class="fs-5 mb-5">
+        <div class="row">
+            <div class="col aquamarine-100">Id: {{ $incidencia->id }}</div>
+        </div>
+        <div class="row">
+            <div class="col aquamarine-100">Tipo: {{ $incidencia->tipo }}</div>
+            <div class="col aquamarine-100">Subtipo: {{ $incidencia->subtipo->subtipo_nombre }}</div>
+        </div>
+        <div class="row">
+            <div class="col aquamarine-100">Fecha de creación: {{ $incidencia->fecha_creacion }}</div>
+            <div class="col aquamarine-100">Fecha de cierre: {{ $incidencia->fecha_cierre }}</div>
+        </div>
+        <div class="row">
+            <div class="col aquamarine-100">Estado: {{ $incidencia->estado }}</div>
+            <div class="col aquamarine-100">Prioridad: {{ $incidencia->prioridad }}</div>
+        </div>
+        <div class="row">
+            <textarea name="" id="" cols="30" rows="10" class="col">{{ $incidencia->descripcion }}</textarea>
+            <textarea name="" id="" cols="30" rows="10" class="col">{{ $incidencia->actuaciones }}</textarea>
+        </div>
 
-    <ul class="list-group fs-5 mb-4">
-        <li class="list-group-item aquamarine-200"">Id: {{ $incidencia->id }}</li>
-        <li class="list-group-item aquamarine-200"">Tipo: {{ $incidencia->tipo }}</li>
-        <li class="list-group-item aquamarine-200"">Subtipo: {{ $incidencia->subtipo_id }}</li>
-        <li class="list-group-item aquamarine-200"">Fecha de creación: {{ $incidencia->fecha_creacion }}</li>
-        <li class="list-group-item aquamarine-200"">Fecha de cierre: {{ $incidencia->fecha_cierre }}</li>
-        <li class="list-group-item aquamarine-200"">Duración: {{ $incidencia->duracion }}</li>
-        <li class="list-group-item aquamarine-200"">Descripción: {{ $incidencia->descripcion }}</li>
-        <li class="list-group-item aquamarine-200"">Actuaciones: {{ $incidencia->actuaciones }}</li>
-        <li class="list-group-item aquamarine-200"">Estado: {{ $incidencia->estado }}</li>
-        <li class="list-group-item aquamarine-200"">Prioridad: {{ $incidencia->prioridad }}</li>
-        <li class="list-group-item aquamarine-200"">Estado: {{ $incidencia->adjunto_url }}</li>
-        <li class="list-group-item aquamarine-200"">Estado: {{ $incidencia->creador->nombre }}</li>
-        <li class="list-group-item aquamarine-200"">Estado: {{ $incidencia->responsable }}</li>
-        <li class="list-group-item aquamarine-200"">Estado: {{ $incidencia->equipo }}</li>
-        <li class="list-group-item aquamarine-200"">Estado: {{ $incidencia->created_at }}</li>
-        <li class="list-group-item aquamarine-200"">Estado: {{ $incidencia->updated_at }}</li>
-    </ul>
+        <ul class="list-group">
+            <li class="list-group-item aquamarine-100">Duración: {{ $incidencia->duracion }}</li>
+            <li class="list-group-item aquamarine-100">Archivo adjunto: {{ $incidencia->adjunto_url }}</li>
+            <li class="list-group-item aquamarine-100">Creador:
+                {{ $incidencia->creador->nombre . ' ' . $incidencia->creador->apellido1 . ' ' . $incidencia->creador->apellido2 }}
+            </li>
+            <li class="list-group-item aquamarine-100">Responsable:
+                {{ $incidencia->responsable->nombre . ' ' . $incidencia->responsable->apellido1 . ' ' . $incidencia->responsable->apellido2 }}
+            </li>
+            <li class="list-group-item aquamarine-100">Equipo: {{ $incidencia->equipo }}</li>
+            <li class="list-group-item aquamarine-100">Creador el día: {{ $incidencia->created_at }}</li>
+            <li class="list-group-item aquamarine-100">Actualizado el día: {{ $incidencia->updated_at }}</li>
+        </ul>
+    </div>
 @endsection
