b1716a59760d98ca93b8a9d89e095e21eed50f0d
cesartg11 <cesar.torregonzalez@iesmiguelherrero.com>
2024-02-24 02:12:07 +0100

    Hcaer método store IncidenciaController Fixes #73

--- app/Http/Controllers/IncidenciaController.php
+++ app/Http/Controllers/IncidenciaController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CrearIncidenciaRequest;
 use App\Models\Incidencia;
+use App\Models\IncidenciaSubtipo;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IncidenciaController extends Controller
 {
@@ -32,10 +36,58 @@ class IncidenciaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CrearIncidenciaRequest $request)
     {
-        //
+        try {
+            DB::beginTransaction();
+
+            $incidencia = new Incidencia();
+
+            $incidencia->id = $request->id;
+            $incidencia->tipo = $request->tipo;
+
+            /*Comprobar si lo de abajo (sacar id subincidencia) funciona, ya que el subsubtipo puede ser null e igual da fallo*/
+
+            // Buscar el ID de la subincidencia, segun tipo, subtipo y subsubtipo elegido, en la tabla incidencias_subtipos
+            $incidencia_subtipo = IncidenciaSubtipo::where('tipo', $request->tipo)
+                ->where('subtipo_nombre', $request->subtipo)
+                ->where('sub_subtipo', $request->subsubtipo)
+                ->firstOrFail();
+
+            // Obtener el ID
+            $idSubincidencia = $incidencia_subtipo->id;
+            //Metemos el id de subincidencia en el campo correspondiente
+            $incidencia->subtipo_id = $idSubincidencia;
+
+            $incidencia->fecha_creacion = $request->fecha_hora;
+            $incidencia->duracion = $request->duracion;
+            $incidencia->descripcion = $request->descripcion;
+            $incidencia->actuaciones = $request->actuaciones;
+            $incidencia->estado = $request->estado;
+            $incidencia->prioridad = $request->prioridad;
+            $incidencia->adjunto_url = $request->fichero;
+
+            /*Lo de abajo, falta sacr creador_id, respinsable_id y equipo_id, con los datos que vienen abajo*/
+
+            //$request->nombre;  nombre del creador
+            //$request->asignado; asignadao a x profesor o profesores, esto ojo, no se muy bien como hacer para que sean varios profes
+
+            //$request->departamento;
+            //$request->num_etiqueta;
+            //$request->aula;
+            //$request->puesto;
+
+            $incidencia->save();
+
+            DB::commit();
+
+            return redirect()->route('incidencias.show', compact('incidencia'))->with('success', 'Incidencia creado correctamente.');
+        } catch (Exception $e) {
+            DB::rollBack();
+            return redirect()->route('incidencias.index')->with('error', 'No se pudo crear la incidencia. Detalles: ' . $e->getMessage());
+        }
     }
+
 
     /**
      * Display the specified resource.
--- app/Http/Requests/CrearIncidenciaRequest.php
+++ app/Http/Requests/CrearIncidenciaRequest.php
@@ -29,10 +29,12 @@ class CrearIncidenciaRequest extends FormRequest
             'tipo' => 'required|in:cuentas,equipos,wifi,internet,software',
             'subtipo' => 'required|in:Educantabria, Google Classroom, Dominio, YedraAltavoces,PC,Monitor,Proyector,Pantalla interactiva,Portátil,Impresoras,Iesmiguelherrero, WIECAN, Instalación, Actualización',
             'subsubtipo' => 'in:Ratón, Ordenador, Teclado, Portátil proporcionado por Consejería, Portátil de aula',
+            'duracion' => 'required',
             'descripcion' => 'required|min:5',
-            'estado' => 'required|in:abierta, asignada, en proceso, enviada a Infortec, resuel',
             'actuaciones' => 'required',
+            'estado' => 'required|in:abierta, asignada, en proceso, enviada a Infortec, resuel',
             'prioridad' => 'required|in:baja, media, alta, urgente',
+            'fichero' => 'mimes:jpeg,jpg,png,gif,pdf,doc,docx,xls,xlsx,ppt,pptx,zip',
         ];
     }
 
@@ -53,13 +55,16 @@ class CrearIncidenciaRequest extends FormRequest
             'subtipo.required' => 'El subtipo es obligatorio.',
             'subtipo.in' => 'El sub-tipo debe ser uno del select.',
             'subsubtipo.in' => 'El sub-sub-tipo debe ser uno del select',
+            'duracion.required' => 'La duracion es obligatoria',
             'descripcion.required' => 'La descripción es obligatoria.',
             'descripcion.min' => 'La descripción debe tener almenos 5 caracteres.',
+            'actuaciones.required' => 'Las actuaciones son obligatorias',
             'estado.required' => 'El estado es obligatrio.',
             'estado.in' => 'El estado debe ser uno del select.',
             'estado.required' => 'El estado es obligatrio.',
             'prioridad.required' => 'La prioridad es obligatria.',
             'prioridad.in' => 'La prioridad debe ser una del select.',
+            'fichero.mimes' => 'El fichero adjunto debe tener un formato valido'
         ];
     }
 }
