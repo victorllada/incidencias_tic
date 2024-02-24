f34f7aa7b76dc670174f3f7686398353e0e2e36c
Hugocl01 <hugocayon@gmail.com>
2024-02-24 03:40:26 +0100

    boton para borrar incidencia

--- resources/views/incidencias/index.blade.php
+++ resources/views/incidencias/index.blade.php
@@ -123,9 +123,12 @@
 
             {{-- Botones --}}
             <div class="d-flex align-items-center gap-2">
+
                 {{-- Boton crear incidencia --}}
                 <a class=" btn aquamarine-400  text-white" type="button" href="{{ route('incidencias.create') }}">Crear
                     incidencia</a>
+
+                {{-- Botones para exportar en los distitos formatos --}}
                 <button type="button" class="btn aquamarine-400 text-white">PDF</button>
                 <button type="button" class="btn aquamarine-400 text-white">EXCEL</button>
                 <button type="button" class="btn aquamarine-400 text-white">CSV</button>
@@ -161,6 +164,8 @@
                 <div class="col fw-bolder p-3">
                     Estado
                 </div>
+                <div class="col">
+                </div>
             </div>
 
             {{-- Lista de incidencias --}}
@@ -194,6 +199,15 @@
                                 </div>
                                 <div class="col p-3">
                                     {{ $incidencia->estado }}
+                                </div>
+                                <div class="col p-3 d-flex justify-content-center align-items-center">
+                                    <button type="button" class="btn aquamarine-400 text-white">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                            <path
+                                                d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                                        </svg>
+                                    </button>
                                 </div>
                             </div>
                         </a>
