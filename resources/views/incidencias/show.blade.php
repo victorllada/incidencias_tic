bcd8dff894a1fa6249b40920bc1d39b69862f70f
Hugocl01 <hugocayon@gmail.com>
2024-02-25 18:52:08 +0100

    cambios en el show

--- resources/views/incidencias/show.blade.php
+++ resources/views/incidencias/show.blade.php
@@ -14,16 +14,22 @@
 
         {{-- Información de la incidencia --}}
         <div class="card mb-5 aquamarine-100">
+
+            {{-- Encabezado del la tarjeta --}}
             <div class="card-header p-2">
                 <h4 class="card-title">Detalles de la Incidencia</h4>
             </div>
+
+            {{-- Cuerpo de la tarjeta --}}
             <div class="card-body">
+                {{-- Fila id --}}
                 <div class="row mb-4">
                     <div class="col-lg-4">
                         <span class="fw-bolder">Id:</span> {{ $incidencia->id }}
                     </div>
                 </div>
 
+                {{-- Fila tipo, sub-tipo y sub-sub-tipo --}}
                 <div class="row mb-4">
                     <div class="col-lg-4">
                         <span class="fw-bolder">Tipo:</span> {{ $incidencia->tipo }}
@@ -40,6 +46,7 @@
                     @endisset
                 </div>
 
+                {{-- Fila creador, fecha creacion y fecha cierre --}}
                 <div class="row mb-4">
                     <div class="col-lg-4">
                         <span class="fw-bolder">Creador:</span>
@@ -53,6 +60,7 @@
                     </div>
                 </div>
 
+                {{-- Fila estado,  prioridad y duracion --}}
                 <div class="row mb-4">
                     <div class="col-lg-4">
                         <span class="fw-bolder">Estado:</span> {{ $incidencia->estado }}
@@ -60,9 +68,13 @@
                     <div class="col-lg-4">
                         <span class="fw-bolder">Prioridad:</span> {{ $incidencia->prioridad }}
                     </div>
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Duración:</span> {{ $incidencia->duracion }}
+                    </div>
                 </div>
 
-                <div class="row mb-3">
+                {{-- Fila equipo --}}
+                <div class="row mb-4">
                     <!-- Si el equipo no es null mostramos sus datos, en caso contrario mostramos mensaje -->
                     <div class="col">
                         @empty($incidencia->equipo)
@@ -70,13 +82,14 @@
                         @else
                             <span class="fw-bolder">Equipo:</span>
                             {{ $incidencia->equipo->tipo_equipo . ' ' . $incidencia->equipo->marca . ' ' . $incidencia->equipo->modelo }}
-                        </div>
-                    @endempty
+                        @endempty
+                    </div>
                 </div>
 
                 <hr>
 
-                <div class="row mb-3">
+                {{-- Fila descripcion y actuaciones --}}
+                <div class="row mb-4">
                     <div class="col-lg-6">
                         <span class="fw-bolder">Descripción:</span>
                         <textarea class="form-control" rows="8">{{ $incidencia->descripcion }}</textarea>
@@ -87,14 +100,15 @@
                     </div>
                 </div>
 
-                <div class="row mb-3">
-                    <div class="col-lg-6 mb-3">
+                {{-- Fila archivo y responsable --}}
+                <div class="row mb-4">
+                    <div class="col-lg-6">
                         <span class="fw-bolder">Archivo adjunto:</span> {{ $incidencia->adjunto_url }}
                     </div>
 
                     <!-- Si responsable no es null, muestra sus atributos nombre y apellidos, si es null muestra mensaje -->
-                    @empty($incidencia->responsables)
-                        <div class="col-lg-6 mb-3">
+                    <div class="col-lg-6">
+                        @empty($incidencia->responsables)
                             <span class="fw-bolder">Responsable:</span> Aún no se ha asignado
                         @else
                             <span class="fw-bolder">Responsable:</span>
@@ -106,17 +120,12 @@
                                     <span>{{ $responsable->nombre_completo }} </span>
                                 @endforeach
                             @endempty
-                        </div>
-                    @endempty
-                </div>
-
-                <div class="row mb-3">
-                    <div class="col-lg-4">
-                        <span class="fw-bolder">Duración:</span> {{ $incidencia->duracion }}
+                        @endempty
                     </div>
                 </div>
 
-                <div class="row mb-3">
+                {{-- Fila creado el dia y actualizado el dia --}}
+                <div class="row mb-4">
                     <div class="col-lg-4">
                         <span class="fw-bolder">Creado el día:</span> {{ $incidencia->created_at }}
                     </div>
@@ -124,11 +133,11 @@
                         <span class="fw-bolder">Actualizado el día:</span>{{ $incidencia->updated_at }}
                     </div>
                 </div>
-                <div class="row mt-5">
-                    <div class="col-1">
+
+                {{-- Botones actilizar y borrar incidencia --}}
+                <div class="row">
+                    <div class="d-flex gap-2">
                         <button type="button" class="btn aquamarine-400 text-white">Actualizar</button>
-                    </div>
-                    <div class="col-2">
                         <button type="button" class="btn aquamarine-400 text-white">Borrar</button>
                     </div>
                 </div>
