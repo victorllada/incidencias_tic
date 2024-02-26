8575921c2f331581de2eee6b360db16c883b91c6
Hugocl01 <hugocayon@gmail.com>
2024-02-26 22:50:06 +0100

    Estilo login Fixes #111

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="background-radial-gradient overflow-hidden" style="height: 100%">
+    <div class="background-radial-gradient overflow-hidden d-flex align-items-center" style="height: 100vh;">
         <style>
+            body {
+                overflow: hidden;
+            }
+
+            .bg-glass {
+                background-color: hsla(0, 0%, 100%, 0.8) !important;
+                backdrop-filter: saturate(200%) blur(25px);
+            }
+
             .background-radial-gradient {
                 background-color: hsl(218, 41%, 15%);
                 background-image: radial-gradient(650px circle at 0% 0%,
@@ -17,14 +26,16 @@
                         hsl(218, 41%, 20%) 75%,
                         hsl(218, 41%, 19%) 80%,
                         transparent 100%);
+                overflow: hidden;
             }
 
             #radius-shape-1 {
+                border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
                 height: 220px;
                 width: 220px;
                 top: -60px;
                 left: -130px;
-                background: radial-gradient(#00686b, #ad1fff);
+                background: radial-gradient(#38b1f8, #ffffff);
                 overflow: hidden;
             }
 
@@ -32,24 +43,61 @@
                 border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
                 bottom: -60px;
                 right: -110px;
-                width: 300px;
+                width: 280px;
                 height: 300px;
-                background: radial-gradient(#44006b, #ad1fff);
+                background: radial-gradient(#b444f5, #60bffb);
                 overflow: hidden;
             }
 
-            .bg-glass {
-                background-color: hsla(0, 0%, 100%, 0.9) !important;
-                backdrop-filter: saturate(200%) blur(25px);
+            #radius-shape-3 {
+                border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
+                bottom: -60px;
+                right: -110px;
+                width: 300px;
+                height: 300px;
+                background: radial-gradient(#44006b, #60bffb);
+                overflow: hidden;
+            }
+
+            @keyframes rotateAnimation {
+                0% {
+                    transform: rotate(0deg);
+                }
+
+                100% {
+                    transform: rotate(360deg);
+                }
+            }
+
+            @keyframes scaleAnimation {
+                0% {
+                    transform: scale(1);
+                }
+
+                50% {
+                    transform: scale(1.2);
+                }
+
+                100% {
+                    transform: scale(1);
+                }
+            }
+
+            #radius-shape-1.animate {
+                animation: rotateAnimation 15s infinite;
+            }
+
+            #radius-shape-2.animate {
+                animation: rotateAnimation 30s infinite;
             }
         </style>
 
         <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
             <div class="row gx-lg-5 align-items-center mb-5">
                 <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
-                    <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
-                        The best offer <br />
-                        <span style="color: hsl(218, 81%, 75%)">for your business</span>
+                    <h1 class="my-5 display-5 fw-bold ls-tight" style="color: #ffffff">
+                        IES
+                        <span style="color: #60bffb">Miguel Herrero</span>
                     </h1>
                     <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -57,11 +105,14 @@
                         dolorum consequatur nulla, neque debitis eos reprehenderit quasi
                         ab ipsum nisi dolorem modi. Quos?
                     </p>
+
+                    <div id="radius-shape-1" class="position-absolute shadow-5-strong animate"></div>
                 </div>
 
                 <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
-                    <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
-                    <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+                    <div id="radius-shape-1" class="position-absolute shadow-5-strong animate"></div>
+                    <div id="radius-shape-1" class="position-absolute shadow-5-strong animate"></div>
+                    <div id="radius-shape-2" class="position-absolute shadow-5-strong animate"></div>
 
                     <div class="card bg-glass">
                         <div class="card-body px-4 py-5 px-md-5">
@@ -98,9 +149,9 @@
                                     </div>
                                 </div>
 
-                                <div class="row mb-3">
-                                    <div class="col-md-6 offset-md-4">
-                                        <div class="form-check">
+                                <div class="form-outline mb-3">
+                                    <div class="col d-flex justify-content-center">
+                                        <div class="form-check d-flex gap-2">
                                             <input class="form-check-input" type="checkbox" name="remember" id="remember"
                                                 {{ old('remember') ? 'checked' : '' }}>
 
@@ -111,8 +162,8 @@
                                     </div>
                                 </div>
 
-                                <div class="row mb-0">
-                                    <div class="col-md-8 offset-md-4">
+                                <div class="form-outline">
+                                    <div class="col d-flex justify-content-center">
                                         <button type="submit" class="btn btn-primary">
                                             {{ __('Login') }}
                                         </button>
