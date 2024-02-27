bae23c55db8b12ec7e325dde47df53df7f8563c8
Luis Concha Cabrales <lconchac01@educantabria.es>
2024-02-27 14:59:51 +0100

    arreglo del metodo create

--- app/Http/Controllers/IncidenciaController.php
+++ app/Http/Controllers/IncidenciaController.php
@@ -35,6 +35,16 @@ class IncidenciaController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $usuarios = User::all();
+        $aulas = Aula::all();
+        return view('incidencias.create',compact('usuarios', 'aulas'));
+    }
+
+    /**
      * Store a newly created resource in storage.
      */
     public function store(CrearIncidenciaRequest $request)
