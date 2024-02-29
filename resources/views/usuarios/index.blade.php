670b856fa917c357a5884bad55cb00a7995b5957
Hugocl01 <hugocayon@gmail.com>
2024-02-29 12:52:11 +0100

    cambios las vistas para los usuarios

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -5,6 +5,261 @@
 @endsection
 @section('contenido')
 
-<h1>Uusarios</h1>
+    <h1>Usuarios</h1>
 
+    <div class="container">
+
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                Hubo errores al rellenar el formulario:
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        {{-- Contenedor del boton filtrar y los botones exportar --}}
+        <div class="d-flex justify-content-between align-items-center gap-3 mb-5">
+            <div class="d-flex gap-2">
+
+                {{-- Boton y desplegable para los filtros --}}
+                <div class="navbar bg-body-tertiary py-0" aria-label="Light offcanvas navbar">
+                    <div>
+                        <button class="btn aquamarine-400 text-white" type="button" data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasNavbarLight" aria-controls="offcanvasNavbarLight"
+                            aria-label="Toggle navigation">
+                            Filtrar
+                            <i class="bi bi-filter"></i>
+                        </button>
+
+                        {{-- Desplegable con los filtros --}}
+                        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbarLight"
+                            aria-labelledby="offcanvasNavbarLightLabel">
+                            <div class="offcanvas-header mb-2">
+                                <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel">Opciones de filtrado</h5>
+
+                                {{-- Boton para cerrar el desplegable --}}
+                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"></button>
+                            </div>
+
+                            {{-- Contendor con los filtros y boton de filtrado --}}
+                            <div class="offcanvas-body d-flex flex-column justify-between gap-4">
+
+                                {{-- Filtro id --}}
+                                <div class="input-group">
+                                    <label class="col-3 input-group-text aquamarine-200" for="idFiltro">ID</label>
+                                    <input class="form-control" id="idFiltro" name="idFiltro" type="search"
+                                        placeholder="Introduce el ID">
+                                </div>
+
+                                {{-- Filtro usuario --}}
+                                <div class="input-group">
+                                    <label class="col-3 input-group-text aquamarine-200" for="nombreFiltro">Usuario</label>
+                                    <input class="form-control" id="nombreFiltro" name="nombreFiltro" type="search"
+                                        placeholder="Introduce nombre del usuario">
+                                </div>
+
+                                {{-- Filtro tipo --}}
+                                <div class="input-group">
+                                    <label class="col-3 input-group-text aquamarine-200" for="tipoFiltro">Tipo</label>
+                                    <select class="form-select" id="tipoFiltro" name="tipoFiltro">
+                                        <option selected value="-1">Selecciona el tipo</option>
+                                        <option value="EQUIPOS">Equipos</option>
+                                        <option value="CUENTAS">Cuentas</option>
+                                        <option value="WIFI">Wifi</option>
+                                        <option value="INTERNET">Internet</option>
+                                        <option value="SOFTWARE">Software</option>
+                                    </select>
+                                </div>
+
+                                {{-- Filtro subtipo --}}
+                                <div class="input-group">
+                                    <label class="col-3 input-group-text aquamarine-200" for="subtipoFiltro">Subtipo</label>
+                                    <select class="form-select" id="subtipoFiltro" name="subtipoFiltro">
+                                        <option selected value="-1">Selecciona el subtipo</option>
+                                    </select>
+                                </div>
+
+                                {{-- Filtro prioridad --}}
+                                <div class="input-group">
+                                    <label class="col-3 input-group-text aquamarine-200"
+                                        for="prioridadFiltro">Prioridad</label>
+                                    <select class="form-select" id="prioridadFiltro" name="prioridadFiltro">
+                                        <option selected value="-1">Selecciona la prioridad</option>
+                                        <option value="BAJA">Baja</option>
+                                        <option value="URGENTE">Urgente</option>
+                                        <option value="MEDIA">Media</option>
+                                        <option value="ALTA">Alta</option>
+                                    </select>
+                                </div>
+
+                                {{-- Filtro fecha desde hasta --}}
+                                <div class="input-group">
+                                    <label class="col-3 input-group-text aquamarine-200"
+                                        for="fechaDesdeFiltro">Fecha</label>
+                                    <input class="form-control" type="date" id="fechaDesdeFiltro" name="fechaDesdeFiltro"
+                                        aria-label="Desde">
+                                    <label class="" for="fechaHastaFiltro"></label>
+                                    <input class="form-control" type="date" id="fechaHastaFiltro" name="fechaHastaFiltro"
+                                        aria-label="Hasta">
+                                </div>
+
+                                {{-- Filtro estado --}}
+                                <div class="input-group">
+                                    <label class="col-3 input-group-text aquamarine-200" for="estadoFiltro">Estado</label>
+                                    <select class="form-select" id="estadoFiltro" name="estadoFiltro">
+                                        <option selected value="-1">Selecciona el estado</option>
+                                        <option value="ABIERTA">Abierta</option>
+                                        <option value="CERRADA">Cerrada</option>
+                                        <option value="RESUELTA">Resuelta</option>
+                                        <option value="ASIGNADA">Asignada</option>
+                                        <option value="ENVIADA A INFORTEC">Enviada a Infortec</option>
+                                        <option value="EN PROCESO">En proceso</option>
+                                    </select>
+                                </div>
+
+                                {{-- Boton de filtrado y borrar filtros --}}
+                                <div class="d-flex justify-content-between gap-2">
+                                    <button class="btn aquamarine-400 text-white" type="submit" id="borrar">Borrar
+                                        filtros</button>
+                                    <button class="btn aquamarine-400 text-white flex-fill" type="submit"
+                                        data-bs-dismiss="offcanvas" id="filtrar">Filtrar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Boton crear usuario --}}
+                <a class=" btn aquamarine-400 text-white" type="button" href="{{ route('usuarios.create') }}">
+                    Crear usuario
+                    <i class="bi bi-plus-lg"></i>
+                </a>
+            </div>
+        </div>
+
+        {{-- Contenedor para el encabezado de y todas las incidencias --}}
+        <div class="container text-left mb-5">
+
+            {{-- Encabezado de la lista de incidencias --}}
+            <div class="row d-flex justify-content-between flex-nowrap text-white aquamarine-300">
+                <div class="col fw-bolder p-3 baja-res">
+                    ID
+                </div>
+                <div class="col fw-bolder p-3 baja-res">
+                    Usuario
+                </div>
+                <div class="col fw-bolder p-3">
+                    Tipo
+                </div>
+                <div class="col fw-bolder p-3">
+                    Subtipo
+                </div>
+                <div class="col fw-bolder p-3 baja-res">
+                    Fecha de creación
+                </div>
+                <div class="col fw-bolder p-3">
+                    Prioridad
+                </div>
+                <div class="col fw-bolder p-3">
+                    Estado
+                </div>
+                <div class="col fw-bolder p-3 movil-res">
+                </div>
+            </div>
+
+            {{-- <a href="{{ route('incidencias.show', $incidencia) }}"></a> --}}
+            {{-- Lista de incidencias --}}
+            <div class="row mb-6">
+                @forelse ($usuarios as $usuario)
+                    <div class="lista-incidencias">
+                        <div class="row d-flex justify-content-between align-items-center flex-nowrap rounded">
+                            <div class="col p-3" onclick="redirect('{{ route('usuarios.show', $usuario) }}')">
+                                {{ $usuario->id }}
+                            </div>
+                            <div class="col p-3" onclick="redirect('{{ route('usuarios.show', $usuario) }}')">
+                                {{ $usuario->id }}
+                            </div>
+                            <div class="col p-3" onclick="redirect('{{ route('usuarios.show', $usuario) }}')">
+                                {{ $usuario->id }}
+                            </div>
+                            <div class="col p-3" onclick="redirect('{{ route('usuarios.show', $usuario) }}')">
+                                {{ $usuario->id }}
+                            </div>
+                            <div class="col p-3" onclick="redirect('{{ route('usuarios.show', $usuario) }}')">
+                                {{ $usuario->id }}
+                            </div>
+                            <div class="col p-3" onclick="redirect('{{ route('usuarios.show', $usuario) }}')">
+                                {{ $usuario->id }}
+                            </div>
+                            <div class="col p-3" onclick="redirect('{{ route('usuarios.show', $usuario) }}')">
+                                {{ $usuario->id }}
+                            </div>
+                            <div class="col p-3 movil-res">
+                                <div class="d-flex flex-column justify-content-center gap-2">
+                                    <a class=" btn aquamarine-400 text-white" type="button"
+                                        href="{{ route('usuarios.show', $usuario) }}">Detalles</a>
+                                    <form action="" class="d-flex">
+                                        <input type="button" class="btn aquamarine-400 text-white flex-fill"
+                                            value="Borrar" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <p>No hay incidencias que mostrar.</p>
+                @endforelse
+            </div>
+        </div>
+
+        {{-- Paginacion --}}
+        <ul class="pagination d-flex justify-content-center mb-5">
+            <li class="page-item">
+                <a class="page-link text-aquamarine-400" href="#" id="inicioPaginacion">Inicio</a>
+            </li>
+            <li class="page-item">
+                <button class="page-link text-aquamarine-400" id="anterior">
+                    <i class="bi bi-arrow-left"></i>
+                </button>
+            </li>
+            <li class="page-item d-flex justify-content-center align-items-center numero-pagina border">
+                <input type="text" id="paginaActual" value="1" class="text-aquamarine-400 input-numero"
+                    size="1">
+                <span id="paginasTotales" class="text-aquamarine-400"></span>
+            </li>
+            <li class="page-item">
+                <button class="page-link text-aquamarine-400" id="siguiente">
+                    <i class="bi bi-arrow-right"></i>
+                </button>
+            </li>
+            <li class="page-item">
+                <button class="page-link text-aquamarine-400" id="finalPaginacion">Final</button>
+            </li>
+        </ul>
+
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Petición de borrado</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Desea borrar la incidencia con id: <span id="numeroID"></span>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="button" class="btn btn-secondary" data-bs-dismiss="modal" value="Cancelar">
+                        <input type="button" class="btn btn-danger" value="Borrar" id="activarBorrado"
+                            name="activarBorrado">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -5,6 +5,150 @@
 @endsection
 @section('contenido')
 
-<h1>Uusarios Show</h1>
+    <h1>Usuarios Show</h1>
 
+    <div class="container">
+
+        @if (session('success'))
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <div class="d-flex justify-content-between align-items-center gap-3 mb-5">
+            {{-- Migas de pan --}}
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('usuarios.index', $usuario) }}">Administrar usuarios</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ $usuario->nombre_completo }}</li>
+                </ol>
+            </nav>
+        </div>
+        {{-- Información de la incidencia --}}
+        <div class="card mb-5 aquamarine-100">
+
+            {{-- Encabezado del la tarjeta --}}
+            <div class="card-header p-2">
+                <h4 class="card-title">Detalles del usuario</h4>
+            </div>
+
+            {{-- Cuerpo de la tarjeta --}}
+            <div class="card-body">
+                {{-- Fila  --}}
+                <div class="row mb-4">
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Id:</span> {{ $usuario->nombre_completo }}
+                    </div>
+                </div>
+
+                {{-- Fila --}}
+                <div class="row mb-4">
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Tipo:</span> {{ $usuario->tipo }}
+                    </div>
+
+                    <!-- Si no hay sub-tipos de usuario no se muestra -->
+                    @isset($incidencia->subtipo->subtipo_nombre)
+                        <div class="col-lg-4">
+                            <span class="fw-bolder">Subtipo:</span> {{ $usuario->subtipo->subtipo_nombre }}
+                        </div>
+                    @endisset
+
+                    <!-- Si no hay sub-sub-tipos de usuario no se muestra -->
+                    @isset($incidencia->subtipo->sub_subtipo)
+                        <div class="col-lg-4">
+                            <span class="fw-bolder">Subsubtipo:</span> {{ $usuario->subtipo->sub_subtipo }}
+                        </div>
+                    @endisset
+                </div>
+
+                {{-- Fila  --}}
+                <div class="row mb-4">
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Creador:</span>
+                    </div>
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Fecha de creación:</span> {{ $usuario }}
+                    </div>
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Fecha de cierre:</span> {{ $usuario }}
+                    </div>
+                </div>
+
+                {{-- Fila --}}
+                <div class="row mb-4">
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Estado:</span> {{ $usuario->estado }}
+                    </div>
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Prioridad:</span> {{ $usuario->prioridad }}
+                    </div>
+                    <div class="col-lg-4">
+                        <span class="fw-bolder">Duración:</span> {{ $usuario->duracion }}
+                    </div>
+                </div>
+
+                {{-- Fila --}}
+                <div class="row mb-4">
+                    <!-- Si el equipo no es null mostramos sus datos, en caso contrario mostramos mensaje -->
+                    <div class="col">
+                        @empty($usuario->equipo)
+                            <span class="fw-bolder">Equipo:</span> No hay equipo asignado
+                        @else
+                            <span class="fw-bolder">Equipo:</span>
+                            {{ $usuario->equipo->tipo_equipo . ' ' . $usuario->equipo->marca . ' ' . $usuario->equipo->modelo }}
+                        @endempty
+                    </div>
+                </div>
+
+                <hr>
+
+                {{-- Fila  --}}
+                <div class="row mb-4">
+                    <div class="col-lg-6">
+                        <span class="fw-bolder">Descripción:</span>
+                        <textarea class="form-control" rows="8" readonly>{{ $usuario->descripcion }}</textarea>
+                    </div>
+                    <div class="col-lg-6">
+                        <span class="fw-bolder">Actuaciones:</span>
+                        <textarea class="form-control" rows="8" readonly>{{ $usuario->actuaciones }}</textarea>
+                    </div>
+                </div>
+
+                {{-- Fila --}}
+                <div class="row mb-4">
+                    <div class="col-lg-6">
+                        <span class="fw-bolder">Archivo adjunto:</span> {{ $usuario->adjunto_url }}
+                    </div>
+
+                    <!-- Si responsable no es null, muestra sus atributos nombre y apellidos, si es null muestra mensaje -->
+                    <div class="col-lg-6">
+                        @empty($usuario->responsables)
+                            <span class="fw-bolder">Responsable:</span> Aún no se ha asignado
+                        @else
+                            <span class="fw-bolder">Responsable:</span>
+                            <!-- Si el nombre o los apellidos es null ponemos info incompleta, en caso contrario lo mostramos -->
+                            @empty($responsables)
+                                Aún no hay responsables
+                            @else
+                                @foreach ($responsables as $responsable)
+                                    <span>{{ $responsable->nombre_completo }} </span>
+                                @endforeach
+                            @endempty
+                        @endempty
+                    </div>
+                </div>
+
+                {{-- Botones actilizar y borrar incidencia --}}
+                <div class="row">
+                    <div class="d-flex gap-2">
+                        <a href="{{ route('usuarios.edit', $usuario) }}" type="button"
+                            class="btn aquamarine-400 text-white">Editar</a>
+                        <button type="button" class="btn aquamarine-400 text-white">Borrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
