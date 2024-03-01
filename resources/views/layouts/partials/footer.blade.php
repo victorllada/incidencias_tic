a8c52c66930577ab3ece2fdd3eda1b1bbf2ab15a
Hugocl01 <hugocayon@gmail.com>
2024-03-01 16:46:42 +0100

    Media query header y footer Fixes #214

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,11 +1,15 @@
 <footer class="aquamarine-900 d-flex flex-column justify-content-center align-items-center">
     <div class="container">
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <p class="col-md-4 mb-0 text-light">Equipo1© 2024</p>
+            <div class="col-md-4 justify-content-start">
+                <span class="col-md-4 mb-0 text-light">Equipo1© 2024</span>
+            </div>
 
-            <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto">
-                <img src="{{ asset('assets/imagenes/logo.svg') }}" alt="logo" class="logo-header">
-            </a>
+            <div class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto">
+                <a href="/" class="">
+                    <img src="{{ asset('assets/imagenes/logo.svg') }}" alt="logo" class="logo-header">
+                </a>
+            </div>
 
             <ul class="nav col-md-4 justify-content-end">
                 <li class="nav-item">
