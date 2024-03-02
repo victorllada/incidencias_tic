86b98f8e5768f0dd0edb6450798fb7347a1b7e7c
Hugocl01 <hugocayon@gmail.com>
2024-03-02 18:40:41 +0100

    Modal para actualizar los datos del usuario Fixes #268

--- resources/views/usuarios/edit.blade.php
+++ resources/views/usuarios/edit.blade.php
@@ -113,6 +113,26 @@
                     </div>
                 </div>
             </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Actualización de usuario</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ¿Estas seguro de guardar los cambios?
+                        </div>
+                        <div class="modal-footer">
+                            <input type="button" class="btn btn-secondary" data-bs-dismiss="modal" value="Cancelar">
+                            <input type="submit" class="btn aquamarine-400 text-white" value="Actualizar">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </form>
     </div>
 @endsection
