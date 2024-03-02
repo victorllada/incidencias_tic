f05e1d42fd0fdb85d6ca08c01524c8961e311723
cesartg11 <cesar.torregonzalez@iesmiguelherrero.com>
2024-03-02 20:24:32 +0100

    Arreglo show y vite Fixes #274

--- resources/views/incidencias/show.blade.php
+++ resources/views/incidencias/show.blade.php
@@ -1,57 +1,27 @@
 @extends('layouts.plantilla')
 @section('titulo', 'Incidencias - show')
 @section('archivosJS')
-    @vite(['resources/js/app.js','resources/js/showIncidencia.js'])
+    @vite(['resources/js/app.js'])
 @endsection
 @section('contenido')
 
     <div>
         <div>
 
-        @if ($errors->any())
-            <div class="alert alert-danger" role="alert">
-                Hubo errores al mandar el mensaje:
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-        @if (session('success'))
-            <div class="alert alert-success" role="alert">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        <div class="d-flex justify-content-between align-items-center gap-3 mb-5">
-            {{-- Migas de pan --}}
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{ route('incidencias.index', $incidencia) }}">Inicio</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Incidencia {{ $incidencia->id }}</li>
-                </ol>
-            </nav>
-
-            <div class="d-flex gap-2">
-                {{-- Boton para exportar en los distitos formatos --}}
-                <div class="dropdown custom-dropdown">
-                    <button class="btn aquamarine-400 text-white dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Exportar
-                    </button>
-                    <ul class="dropdown-menu custom-dropdown-menu">
-                        <li><a class="dropdown-item"
-                                href="{{ route('incidencias.exportar', ['tipo' => $incidencia->id, 'formato' => 'pdf']) }}">PDF</a>
-                        </li>
-                        <li><a class="dropdown-item"
-                                href="{{ route('incidencias.exportar', ['tipo' => $incidencia->id, 'formato' => 'xlsx']) }}">EXCEL</a>
-                        </li>
-                        <li><a class="dropdown-item"
-                                href="{{ route('incidencias.exportar', ['tipo' => $incidencia->id, 'formato' => 'csv']) }}">CSV</a>
-                        </li>
+            @if ($errors->any())
+                <div class="alert alert-danger" role="alert">
+                    Hubo errores al mandar el mensaje:
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
                     </ul>
+                </div>
+            @endif
+
+            @if (session('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('success') }}
                 </div>
             @endif
 
@@ -104,42 +74,42 @@
                                         aria-label="Close"></button>
                                 </div>
 
+                                {{-- Contendor comentarios --}}
+                                <div class="offcanvas-body d-flex flex-column gap-5 pt-5">
 
-                                @forelse ($comentarios as $comentario)
-                                    <p><span>{{ $comentario->user->nombre_completo }}:</span>
-                                        {{ $comentario->texto }}
-                                        {{-- Si el comentario tiene archivo adjunto se muestra botón para descargar --}}
-                                        @if ($comentario->adjunto_url != null)
-                                            {{-- Maquetar esto bien para que se vea un poquito separado del texto o lo que sea --}}
-                                            <span>Archivo adjunto: <a href="{{ route('descargar.comentario.archivo', ['id' => $comentario->id]) }}"
-                                                class="btn aquamarine-400 text-white">Descargar Archivo</a> <span>
+                                    @forelse ($comentarios as $comentario)
+                                        <p><span>{{ $comentario->user->nombre_completo }}:</span>
+                                            {{ $comentario->texto }}
+                                            {{-- Si el comentario tiene archivo adjunto se muestra botón para descargar --}}
+                                            @if ($comentario->adjunto_url != null)
+                                                {{-- Maquetar esto bien para que se vea un poquito separado del texto o lo que sea --}}
+                                                <span>Archivo adjunto: <a
+                                                        href="{{ route('descargar.comentario.archivo', ['id' => $comentario->id]) }}"
+                                                        class="btn aquamarine-400 text-white">Descargar Archivo</a> <span>
+                                            @endif
+                                        </p>
+                                    @empty
+                                        <p>No hay comentarios</p>
+                                    @endforelse
 
-                                        @endif
-                                    </p>
-                                @empty
-                                    <p>No hay comentarios</p>
-                                @endforelse
-
-
+                                    {{-- Contenedor de envio de mensaje --}}
+                                    <div class="offcanvas-body d-flex flex-column gap-5 pt-5">
+                                        <form action="{{ route('comentarios.store') }}" method="POST"
+                                            enctype="multipart/form-data">
+                                            @csrf
+                                            {{-- Campo escondido para mandar el id de la incidencia --}}
+                                            <input type="hidden" name="incidencia_id" value="{{ $incidencia->id }}">
+                                            {{-- Molaria un div como whatsapp en el que puedas meter mensaje y a la derecha un clip para elegir archivo --}}
+                                            <div>
+                                                <input type="text" name="texto" placeholder="Mensaje">
+                                                <input type="file" id="fichero" name="fichero">
+                                            </div>
+                                            {{-- Botonn de enviar puede ser icono como whatsapp o alguna cosa así --}}
+                                            <button type="submit" class="btn btn-danger text-white">Enviar</button>
+                                        </form>
+                                    </div>
 
                                 </div>
-                            </div>
-
-                            {{-- Contenedor de envio de mensaje --}}
-                            <div class="offcanvas-body d-flex flex-column gap-5 pt-5">
-                                <form action="{{ route('comentarios.store') }}" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    {{-- Campo escondido para mandar el id de la incidencia --}}
-                                    <input type="hidden" name="incidencia_id" value="{{ $incidencia->id }}">
-                                    {{-- Molaria un div como whatsapp en el que puedas meter mensaje y a la derecha un clip para elegir archivo --}}
-                                    <div>
-                                        <input type="text" name="texto" placeholder="Mensaje">
-                                        <input  type="file" id="fichero"
-                                        name="fichero">
-                                    </div>
-                                    {{-- Botonn de enviar puede ser icono como whatsapp o alguna cosa así --}}
-                                    <button type="submit" class="btn btn-danger text-white">Enviar</button>
-                                </form>
                             </div>
                         </div>
                     </div>
@@ -225,63 +195,63 @@
                             {{ $incidencia->equipo->aula->codigo . ' ' . $incidencia->equipo->etiqueta . ' ' . $incidencia->equipo->puesto }}
                         @endempty
                     </div>
-                @endif
+            @endif
 
-                <hr>
+            <hr>
 
-                {{-- Fila descripcion y actuaciones --}}
-                <div class="row mb-4">
-                    <div class="col-lg-6">
-                        <span class="fw-bolder">Descripción:</span>
-                        <textarea class="form-control" rows="8" readonly>{{ $incidencia->descripcion }}</textarea>
-                    </div>
-                    <div class="col-lg-6">
-                        <span class="fw-bolder">Actuaciones:</span>
-                        <textarea class="form-control" rows="8" readonly>{{ $incidencia->actuaciones }}</textarea>
-                    </div>
+            {{-- Fila descripcion y actuaciones --}}
+            <div class="row mb-4">
+                <div class="col-lg-6">
+                    <span class="fw-bolder">Descripción:</span>
+                    <textarea class="form-control" rows="8" readonly>{{ $incidencia->descripcion }}</textarea>
+                </div>
+                <div class="col-lg-6">
+                    <span class="fw-bolder">Actuaciones:</span>
+                    <textarea class="form-control" rows="8" readonly>{{ $incidencia->actuaciones }}</textarea>
+                </div>
+            </div>
+
+            {{-- Fila archivo y responsable --}}
+            <div class="row mb-4">
+                <div class="col-lg-6">
+                    @if ($incidencia->adjunto_url != null)
+                        <a href="{{ route('descargar.archivo', ['id' => $incidencia->id]) }}"
+                            class="btn aquamarine-400 text-white">Descargar Archivo</a>
+                    @else
+                        <span class="fw-bolder">No hay archivo adjunto</span>
+                    @endif
                 </div>
 
-                {{-- Fila archivo y responsable --}}
-                <div class="row mb-4">
-                    <div class="col-lg-6">
-                        @if ($incidencia->adjunto_url != null)
-                            <a href="{{ route('descargar.archivo', ['id' => $incidencia->id]) }}"
-                                class="btn aquamarine-400 text-white">Descargar Archivo</a>
-                        @else
-                            <span class="fw-bolder">No hay archivo adjunto</span>
-                        @endif
-                    </div>
-
-                    <div class="col-lg-6">
-                        <span class="fw-bolder">Responsable:</span>
-                        <!-- Si responsables esta vacio ponemos que aún no hay, en caso contrario los mostramos -->
-                        @if ($incidencia->responsable == null)
-                            Aún no hay responsables
-                        @else
-                            <span>{{ $incidencia->responsable->nombre_completo }} </span>
-                        @endif
-                    </div>
+                <div class="col-lg-6">
+                    <span class="fw-bolder">Responsable:</span>
+                    <!-- Si responsables esta vacio ponemos que aún no hay, en caso contrario los mostramos -->
+                    @if ($incidencia->responsable == null)
+                        Aún no hay responsables
+                    @else
+                        <span>{{ $incidencia->responsable->nombre_completo }} </span>
+                    @endif
                 </div>
+            </div>
 
-                {{-- Botones actilizar y borrar incidencia --}}
-                <div class="row mt-5">
-                    <div class="d-flex gap-2">
-                        <a href="{{ route('incidencias.edit', $incidencia) }}" type="button"
-                            class="btn aquamarine-400 text-white">Editar</a>
-                        <form action="{{ route('incidencias.destroy', $incidencia) }}" method="POST" id="formBorrar">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-danger text-white"data-bs-toggle="modal"
-                                data-bs-target="#staticBackdrop" id="botonBorrar">Borrar</button>
-                        </form>
-                    </div>
+            {{-- Botones actilizar y borrar incidencia --}}
+            <div class="row mt-5">
+                <div class="d-flex gap-2">
+                    <a href="{{ route('incidencias.edit', $incidencia) }}" type="button"
+                        class="btn aquamarine-400 text-white">Editar</a>
+                    <form action="{{ route('incidencias.destroy', $incidencia) }}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-danger text-white"data-bs-toggle="modal"
+                            data-bs-target="#staticBackdrop">Borrar</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
     aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
@@ -294,8 +264,12 @@
             </div>
             <div class="modal-footer">
                 <input type="button" class="btn btn-secondary" data-bs-dismiss="modal" value="Cancelar">
+                <form action="" id="formBorrado" method="POST">
+                    @csrf
+                    @method('delete')
                     <input type="submit" class="btn btn-danger" value="Borrar" id="activarBorrado"
                         name="activarBorrado">
+                </form>
             </div>
         </div>
     </div>
