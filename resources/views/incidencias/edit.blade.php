a1fa19a995e5cff0be5f3bc72c21c719ae0774d7
Hugocl01 <hugocayon@gmail.com>
2024-03-02 17:58:15 +0100

    modales edit

--- resources/views/incidencias/edit.blade.php
+++ resources/views/incidencias/edit.blade.php
@@ -234,6 +234,28 @@
                     {{-- Boton para editar incidencia --}}
                     <div class="row mt-5">
                         <div class="col">
+                            <button type="button" class="btn aquamarine-400 text-white"data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop">Actualizar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Actualización de datos</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ¿Estas seguro de guardar los cambios?
+                        </div>
+                        <div class="modal-footer">
+                            <input type="button" class="btn btn-secondary" data-bs-dismiss="modal" value="Cancelar">
                             <input type="submit" class="btn aquamarine-400 text-white" value="Actualizar">
                         </div>
                     </div>
