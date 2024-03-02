955d9a9cf101f470f818bffe8c4e9b11cf871c32
Luis Concha Cabrales <lconchac01@educantabria.es>
2024-03-03 00:39:48 +0100

    cambio de ruta a edit

--- resources/js/usuarios.js
+++ resources/js/usuarios.js
@@ -363,7 +363,7 @@ function generarUsuarios(datos)
     for(let i=0;datos[pagina].length;i++)
     {
         //ruta para la vista de show de esa incidencia
-        let stringRedirect=ruta+"/"+datos[pagina][i].usuario;
+        let stringRedirect=ruta+"/"+datos[pagina][i].usuario+"/edit";
 
         //creo el div padre de la incidencia y le doy las clases necesarias
         let divPadre=document.createElement("div");//contenedor de la incidencia
